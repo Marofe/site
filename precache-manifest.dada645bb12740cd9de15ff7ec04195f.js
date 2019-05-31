c2db31cf28414a62035648ba827c24e1a250fd9c
Marcos <eng.marofe@hotmail.com>
2019-05-31 16:21:26 -0300

    first build

---
+++ precache-manifest.dada645bb12740cd9de15ff7ec04195f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c322198894e47cf745e015afbf5e616",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b9a2ad4443f515cab40",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0577eeb67436f18aa332",
+    "url": "/static/js/2.94912eac.chunk.js"
+  },
+  {
+    "revision": "5b9a2ad4443f515cab40",
+    "url": "/static/js/main.a7dc9daa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
