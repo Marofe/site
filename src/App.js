1294152229f93fa143f32d62f6ba359d7ad8fb76
Marcos <eng.marofe@hotmail.com>
2019-05-31 00:42:16 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Layout from './Components/Layout/Layout';
+import {BrowserRouter, Route} from 'react-router-dom';
+import HomePage from './Components/Pages/HomePage';
+import Publications from './Components/Pages/Publications';
+import Tutorials from './Components/Pages/Tutorials';
+import Notes from './Components/Pages/Notes';
+import Header from './Components/Header/Header';
+import Navigation from './Components/Header/Navigation';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Layout>
+      <Header>
+        <h1>Hi! I'm Marcos</h1>
+        <Navigation/>
+      </Header>
+      <BrowserRouter>     
+      <Route path="/" exact component={HomePage}/>
+      <Route path="/publications" exact component={Publications}/>
+      <Route path="/tutorials" exact component={Tutorials}/>
+      <Route path="/notes" exact component={Notes}/>
+      </BrowserRouter>
+    </Layout> 
+  )
 }
 
 export default App;
